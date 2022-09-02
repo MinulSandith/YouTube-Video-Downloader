7d08d06a85f4739089f92e2dc91f8145130f6fd2
Minul Sandith <106053448+MinulSandith@users.noreply.github.com>
2022-09-02 10:35:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from pytube import YouTube
+
+col1,col2=st.columns(2)
+
+reso_list=["360p",'720p','1080p']
+format_list=["mp4"]
+
+st.header("Youtube Video Downloader")
+col1,col2=st.columns(2)
+
+link=col1.text_input("Link")
+
+if col1.button("Find"):
+    try:
+        video=st.video(link)
+    except:   
+    
+        st.error("Can't find such a video.Resons might be poor internet connection , no such video with given resolution and file type or entering an incorrect link ")
+    
+
+req_reso=col2.selectbox("Resolution",reso_list)
+format=col2.selectbox("File type",format_list)
+
+if col2.button("Download") :
+    try:
+        video=YouTube(link).streams.filter(res=req_reso,file_extension = format).first()
+        
+        video.download("C:\YouTube Video Downloads")
+    except:
+        st.error("Poor connection.Try again later")
+     
+     
