91f35ece569581025bf66b39ccf56076a58df76c
Minul Sandith <106053448+MinulSandith@users.noreply.github.com>
2022-09-02 10:46:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if col2.button("Download") :
     try:
         video=YouTube(link).streams.filter(res=req_reso,file_extension = format).first()
         
-        video.download("C:\YouTube Video Downloads")
+        video.download(""C:\Users\minul\Videos"")
     except:
         st.error("Poor connection.Try again later")
      
